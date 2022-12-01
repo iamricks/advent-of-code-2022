9a681afbcba6a124381ad7d35341690eff4c8475
iamricks <richardsantos@Richards-MacBook-Pro.local>
2022-12-01 00:26:46 -0500

    part 2 of day 1 done

--- Day 1/main.rb
+++ Day 1/main.rb
@@ -1,3 +1,14 @@
 # Author @ricks
-answer = "something"
-print(answer)
+all_elf_cals = [2000,0]
+
+File.open("/Users/richardsantos/Desktop/dev/advent-of-code-2022/Day 1/input.txt", "r") do |file_handle|
+  file_handle.each_line do |calories|
+    if calories.strip != ""
+      all_elf_cals[-1] += calories.to_i
+    else
+      all_elf_cals << 0
+    end
+  end
+end
+
+print(all_elf_cals.sort.last(3).sum)
